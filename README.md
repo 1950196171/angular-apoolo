892433c7c5c45b4584525f30707eec0f4ee57ed1
王一凡 <wangyifan@czxy.com>
2019-01-21 17:52:21 +0800

    '代码注释'

--- README.md
+++ README.md
@@ -32,3 +32,4 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 ## Password：anheng
 
+##有点小毛病，具体看代码O.O
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -12,11 +12,15 @@ export class UsersComponent implements OnInit {
   password:'';
   // 定义show为空
   show:'';
-  user:any[];
+  user:{};
   //初始化 
   constructor(private apollo: Apollo) { }
-  //生命周期函数 直接去拿数据
+  
   ngOnInit() {
+  //   第一次启动 会有点问题  
+  //   启动服务器后 等待网页出现 Cannot GET / 后  请将以下代码注释 等页面加载出来后    再将这段代码打开
+
+    //   开始注释
     this.apollo
       .watchQuery({
       query: gql`
@@ -33,10 +37,14 @@ export class UsersComponent implements OnInit {
     .valueChanges.subscribe(result => {
       this.user=result.data.users;
     });
+    //结束注释
+
+
+    
   }
 login():void{
   //判断点击完按钮  用户输入的值是否和请求回来的值一致
-  if(this.username== this.user[1].username && this.password==this.user[1].password){
+  if(this.username==this.user[1].username && this.password==this.user[1].password){
       this.show = 'true';
   }else{
     alert('用户名或密码错误！');
